f447a397ecd8ed76ba62e7226a5b70c0ea813118
Mohammed-S-Sheikh <muhammadsaaher@gmail.com>
2025-08-28 12:59:23 +0200

    tab bar component

---
+++ lib/src/ui/widget/helper_tab_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:helper/src/ui/context_extension.dart';
+
+class HelperTabBar extends StatelessWidget implements PreferredSizeWidget {
+  const HelperTabBar({
+    super.key,
+    required this.tabs,
+    this.onTap,
+    this.controller,
+    this.indicatorColor,
+    this.labelColor,
+    this.unselectedlabelColor,
+    this.preferredSizeHeight = kToolbarHeight,
+  });
+
+  final List<String> tabs;
+  final ValueChanged<int>? onTap;
+  final TabController? controller;
+  final Color? indicatorColor;
+  final Color? labelColor;
+  final Color? unselectedlabelColor;
+  final double preferredSizeHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      clipBehavior: Clip.hardEdge,
+      decoration: BoxDecoration(
+        color: context.colorScheme.surfaceContainer,
+        borderRadius: BorderRadius.circular(24),
+      ),
+      child: TabBar(
+        onTap: onTap,
+        controller: controller,
+        dividerColor: Colors.transparent,
+        overlayColor: WidgetStatePropertyAll(Colors.transparent),
+        indicatorSize: TabBarIndicatorSize.tab,
+        indicatorPadding: const EdgeInsets.all(6),
+        indicator: BoxDecoration(
+          color: indicatorColor ?? context.colorScheme.primary,
+          borderRadius: BorderRadius.circular(24),
+        ),
+        unselectedLabelColor: unselectedlabelColor,
+        labelStyle: context.textTheme.labelLarge
+            ?.copyWith(color: labelColor ?? context.colorScheme.onPrimary),
+        tabs: [for (final tab in tabs) Tab(text: tab)],
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(preferredSizeHeight);
+}
--- lib/src/ui/widget/widget.dart
+++ lib/src/ui/widget/widget.dart
@@ -2,5 +2,6 @@ export 'conditional_wrapper.dart';
 export 'filled_icon.dart';
 export 'header_widget.dart';
 export 'helper_bottom_sheet.dart';
+export 'helper_tab_bar.dart';
 export 'otp_page.dart';
 export 'outlined_icon.dart';
