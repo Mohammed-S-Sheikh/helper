393ac682b3b90bb1f5c654125f4c858e15e7339f
Mohammed-S-Sheikh <muhammadsaaher@gmail.com>
2026-01-06 12:37:41 +0200

    refactor: remove unnecessary WidgetsFlutterBinding initialization in appRunner

--- lib/src/app/runner.dart
+++ lib/src/app/runner.dart
@@ -1,5 +1,4 @@
-import 'package:flutter/material.dart'
-    show Widget, WidgetsFlutterBinding, runApp;
+import 'package:flutter/material.dart' show Widget, runApp;
 import 'package:helper/src/app/language.dart';
 import 'package:helper/src/app/theme.dart';
 import 'package:helper/src/logic/data/local/prefs.dart';
@@ -11,8 +10,6 @@ Future<void> appRunner({
   required void Function()? on401,
   required Widget app,
 }) async {
-  WidgetsFlutterBinding.ensureInitialized();
-
   await HelperPrefs.initialize();
   await HelperLanguage().loadSavedLanguage();
   await HelperTheme().loadSavedTheme();
