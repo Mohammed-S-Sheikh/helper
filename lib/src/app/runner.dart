3637b25887d56c4d76b1628b3b7db54df5a30565
Mohammed-S-Sheikh <muhammadsaaher@gmail.com>
2026-01-06 12:32:42 +0200

    feat: update appRunner to accept baseUrl and extraHeaders parameters

--- lib/src/app/runner.dart
+++ lib/src/app/runner.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart'
 import 'package:helper/src/app/language.dart';
 import 'package:helper/src/app/theme.dart';
 import 'package:helper/src/logic/data/local/prefs.dart';
+import 'package:helper/src/logic/data/network/api.dart';
 
-Future<void> appRunner(Widget app) async {
+Future<void> appRunner({
+  required String baseUrl,
+  Map<String, dynamic> Function()? extraHeaders,
+  required void Function()? on401,
+  required Widget app,
+}) async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await HelperPrefs.initialize();
   await HelperLanguage().loadSavedLanguage();
   await HelperTheme().loadSavedTheme();
+  Api.initialize(
+    baseUrl: baseUrl,
+    extraHeaders: extraHeaders,
+    on401: on401,
+  );
 
   runApp(app);
 }
